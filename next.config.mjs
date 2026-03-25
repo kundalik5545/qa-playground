7d940d8b6f03b1d7b17bf17080ef3f6d6587ad74
kundalik5545 <kundalikjadhav0807@gmail.com>
2026-03-25 18:48:33 +0530

    fix: prod issue with create new user not working

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ["@prisma/client", ".prisma/client", "@prisma/adapter-pg"],
+
   images: {
     remotePatterns: [
       {
